364bd5d062a7765369eb947466ef33c15064333a
Vinicius Ribeiro <vmr20@inf.ufpr.br>
2023-06-27 16:56:38 -0300

    quae ok

--- mmul.c
+++ mmul.c
@@ -61,17 +61,27 @@ void mult_sequencial(double *A, double *B, double *C, int nla, int m, int ncb)
 
 int comparar_matrizes(const double *matriz1, const double *matriz2, int linhas, int colunas)
 {
+    for (int i = 0; i < linhas * colunas; i++)
+    {
+        if (matriz1[i] != matriz2[i])
+        {
+            return 0; // diferentes
+        }
+    }
+    return 1; // iguais
+}
+
+void imprimir_matriz(const double *matriz, int linhas, int colunas)
+{
+    printf("Matriz %dx%d:\n", linhas, colunas);
     for (int i = 0; i < linhas; i++)
     {
         for (int j = 0; j < colunas; j++)
         {
-            if (matriz1[i * colunas + j] != matriz2[i * colunas + j])
-            {
-                return 0; // diferentes
-            }
+            printf("%lf ", matriz[i * colunas + j]);
         }
+        printf("\n");
     }
-    return 1; // iguais
 }
 
 int main(int argc, char *argv[])
@@ -89,10 +99,10 @@ int main(int argc, char *argv[])
     int ncb = atoi(argv[3]);
 
     // Verifica se o argumento opcional "-v" foi fornecido
-    int sequencial = 0;
+    int verificador = 0;
     if (argc > 4 && strcmp(argv[4], "-v") == 0)
     {
-        sequencial = 1;
+        verificador = 1;
     }
 
     int nproc, processId;
@@ -115,35 +125,61 @@ int main(int argc, char *argv[])
 
     chronometer_t mult_chrono;
     MPI_Barrier(MPI_COMM_WORLD);
+
     if (processId == 0)
     {
         chrono_reset(&mult_chrono);
         chrono_start(&mult_chrono);
     }
 
-    mult_paralela(A, B, C, nla, m, ncb);
+    if (nproc > 1)
+        mult_paralela(A, B, C, nla, m, ncb);
+    else
+        mult_sequencial(A, B, C, nla, m, ncb);
 
     MPI_Barrier(MPI_COMM_WORLD);
+
     if (processId == 0)
     {
         chrono_stop(&mult_chrono);
-        chrono_reportTime(&mult_chrono, "mult_chrono");
+    }
 
-        double total_time_in_seconds = (double)chrono_gettotal(&mult_chrono) /
-                                       ((double)1000 * 1000 * 1000);
+    if (processId == 0 && verificador && nproc > 1)
+    {
+        mult_sequencial(A, B, C2, nla, m, ncb);
 
-        printf("Tempo total: %lf s\n", total_time_in_seconds);
-
-        if (sequencial)
+        if (comparar_matrizes(C, C2, nla, ncb) == 1)
         {
-            mult_sequencial(A, B, C2, nla, m, ncb);
-
-            if (comparar_matrizes(C, C2, nla, ncb) == 0)
-                printf("Resultado paralelo OK!\n");
-            else
-                printf("ERRO: Resultado paralelo != resultado sequencial!\n");
+            printf("Resultado paralelo OK!\n");
+        }
+        else
+        {
+            printf("ERRO: Resultado paralelo != resultado sequencial!\n");
         }
     }
+    else if (processId == 0 && verificador && nproc == 1)
+    {
+        printf("Com 1 processo executamos apenas a versao sequencial!\n");
+    }
+    else if (processId == 0)
+    {
+        double total_time_in_seconds = (double)chrono_gettotal(&mult_chrono) /
+                                    ((double)1000 * 1000 * 1000);
+
+        printf("Tempo total: %lf s\n", total_time_in_seconds);
+        
+        double GFLOPS = (((double)nla * ncb * m) / ((double)total_time_in_seconds * 1000 * 1000 * 1000));
+		printf("Vazao: %lf GFLOPS\n", GFLOPS);
+    }
+
+    // Get the name of the processor
+	char processor_name[MPI_MAX_PROCESSOR_NAME];
+	int name_len;
+	MPI_Get_processor_name(processor_name, &name_len);
+
+    // Print node info
+    if (!verificador && nproc > 1)
+	    printf("Rank %d no Host %s com %d processos do MPI\n", processId, processor_name, nproc);
 
     free(A);
     free(B);
